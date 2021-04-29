9077e6f1de3f66efc6d911325eaeadf80a241681
Zakirov Rishat <zakirovrishat.me@gmail.com>
2021-04-29 18:15:26 +0300

    rename module test_version -> unit_tests

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,11 +1,11 @@
-#define BOOST_TEST_MODULE test_version
+#define BOOST_TEST_MODULE unit_tests
 
 #include "lib.h"
 #include "ip_filter.h"
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(test_version)
+BOOST_AUTO_TEST_SUITE(unit_tests)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
